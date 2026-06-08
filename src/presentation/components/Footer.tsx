4030e84b00af4cf2a26d16b6a169d76ab2637a9c
jonas.gomes-at-171944429159 <jonasgomespe.2020@gmail.com>
2026-06-08 11:48:11 -0300

    feat: :sparkles: remove o texto do rodape

--- src/presentation/components/Footer.tsx
+++ src/presentation/components/Footer.tsx
@@ -5,11 +5,7 @@ export function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.content}>
-        <p>SprintoLog &copy; {new Date().getFullYear()}</p>
         <div className={styles.madeBy}>
-          <span>Feito com</span>
-          <Heart size={14} className={styles.heart} fill="currentColor" />
-          <span>para alta performance</span>
         </div>
       </div>
     </footer>
